5ac074a9d6feef7c4e9c454ca227882fee0ee824
fpriemesberger <fpreimesberger@gmail.com>
2018-08-10 10:07:42 -0700

    fviovj

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.redirect_uri = process.env.REDIRECT_URI;
 
   // render the error page
   res.status(err.status || 500);
